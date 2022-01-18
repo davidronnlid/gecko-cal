e7a27771a076fd2b8013895f4f627d1deb0fe242
David Rönnlid - Nootralize <dr@nootralize.com>
2022-01-18 21:50:06 +0100

    GeckoCal init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import LoginButton from "./components/auth0Login"
+import LogoutButton from "./components/auth0Logout"
+import Profile from "./components/auth0Profile"
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoginButton />
+      <LogoutButton />
+      <div>{Profile()}</div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="geckocal.eu.auth0.com"
+      clientId="BtXFe3Tjn5a2u28Qzifa0QAJjWJRcZZi"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
